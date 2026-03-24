88f5e6ec38db6868e23304b10e8b59afa0d4426f
Zubtsov <colddry1307@gmail.com>
2026-03-24 18:52:18 +0300

    Initial commit

---
+++ lib/open_alex_wrap.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "open_alex_wrap/version"
+
+module OpenAlexWrap
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "open_alex_wrap"
+
+require "minitest/autorun"
